964e8a38d3fc46bed2f7c7856e620aa531a3b9c5
hareesh425 <hareeshj2ee@gmail.com>
2025-09-05 07:09:31 -0400

    more clean up

--- src/test/java/com/demo/PearlCard/controller/FareCalculationControllerTest.java
+++ src/test/java/com/demo/PearlCard/controller/FareCalculationControllerTest.java
@@ -20,7 +20,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.demo.PearlCard.dto.FareCalculationResponse;
